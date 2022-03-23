2b62b76097d8983bfa39bbc8cca1e3f19dbe5ea5
denny-saryanto <dennysaryanto7@gmail.com>
2022-03-23 13:50:38 +0700

    commit

---
+++ todo/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render, reverse
+from django.http import HttpResponseRedirect
+from django.core.exceptions import ObjectDoesNotExist
+from .models import Todo
+
+# Create your views here.
+def index(request):
+    items = Todo.objects.order_by('-id')
+    return render(request, 'todo/index.html', {'items' : items})
+
+def done(request):
+    items = Todo.objects.filter(status=True).order_by('-id')
+    return render(request, 'todo/index.html', {'items' : items})
+
+def pending(request):
+    items = Todo.objects.filter(status=False).order_by('-id')
+    return render(request, 'todo/index.html', {'items' : items})
+
+def create(request):
+    try:
+        title = request.POST['title'] # Mengambil Data Dari Form dengan Name "Title"
+        todo = Todo(title=title)
+        todo.save()
+        return HttpResponseRedirect(reverse('index'))
+    except Exception:
+        return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+    try:
+        todo = Todo.objects.get(id=id)
+        todo.status = not todo.status
+        todo.save()
+        return HttpResponseRedirect(reverse('index'))
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+    try:
+        todo = Todo.objects.get(id=id)
+        todo.delete()
+        return HttpResponseRedirect(reverse('index'))
+    except ObjectDoesNotExist:
+        return HttpResponseRedirect(reverse('index'))
+
+def delete_all(request):
+    Todo.objects.all().delete()
+    return HttpResponseRedirect(reverse('index'))
